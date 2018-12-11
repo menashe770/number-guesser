e9bceaf29487d6bf28c4ccca102dba309f488577
Niraj Aryal <niroz.aryal11@gmail.com>
2018-12-10 19:02:01 -0700

    displays error messages

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-var randomNumber = 5;
-
 var submit = document.querySelector('.submit-button');
 var rightSide = document.querySelector('.right-side'); 
 var clearButton = document.querySelector('.clear-button');
@@ -20,31 +18,58 @@ var playerOneGuessComment = document.querySelector('.player-one-guess-comment');
 var playerTwoGuessComment = document.querySelector('.player-two-guess-comment');
 var deleteButton = document.querySelector('.delete-button');
 var winnerName = document.querySelector('.winner-name');
+var disableButton = document.querySelectorAll('.disable-button');
+var errorMessage = document.querySelector('.error-img');
+
+
+
+var randomNumber = randomNumberFunction();
+
+
+for (var i=0; i<disableButton.length;i++){
+  disableButton[1].disabled = true;
+  disableButton[2].disabled = true;
+}
+
+
+
+
+
+
 submit.addEventListener('click', function(e){
         	e.preventDefault();
           if(playerOneGuess.value < randomNumber){
             playerOneGuessComment.innerText = "Thats too low";
-          } if(playerTwoGuess.value < randomNumber){
-            playerTwoGuessComment.innerText = "Thats too low";
-          } if(playerOneGuess.value > randomNumber){
-            playerOneGuessComment.innerText = "Thats too high";
-          } if(playerTwoGuess.value > randomNumber){
-            playerTwoGuessComment.innerText = "Thats too high";
-          } if(playerOneGuess.value == randomNumber){
-              playerOneGuessComment.innerText = "Boom!!";
-              appendCard(e);
-              getWinner();
-              
-          } else if(playerTwoGuess.value == randomNumber){
-              playerTwoGuessComment.innerText = "Boom!!";
-              appendCard(e);
-              getWinner();
+          } else if(playerOneGuess.value > randomNumber){
+            playerOneGuessComment.innerText = "Thats too high"} 
+            else{
+            playerOneGuessComment.innerText = "Boom!!";
+            appendCard();
+            getWinner();
           }
+          if(playerTwoGuess.value < randomNumber){
+            playerTwoGuessComment.innerText = "Thats too low";
+          } else if(playerTwoGuess.value > randomNumber){
+            playerTwoGuessComment.innerText = "Thats too high"} 
+            else{
+            playerTwoGuessComment.innerText = "Boom!!";
+            appendCard();
+            getWinner();
+          }
+          disableButton[1].disabled = false;
+          disableButton[2].disabled = false;
+
+
+          
         	
           playerOneNameDisplay.innerText = playerOneName.value;
           playerTwoNameDisplay.innerText = playerTwoName.value;
           playerOneGuessDisplay.innerText = playerOneGuess.value;
           playerTwoGuessDisplay.innerText = playerTwoGuess.value;
+          randomNumberFunction();
+          errorDisplay();
+
+          
           
 
 
@@ -55,6 +80,10 @@ updateButton.addEventListener('click', function(e){
   e.preventDefault();
   minNumber.innerText = minRange.value;
   maxNumber.innerText = maxRange.value;
+  randomNumber = randomNumberFunction();
+  console.log(randomNumber);
+  
+
 })
 
 clearButton.addEventListener('click',function(event){
@@ -63,18 +92,21 @@ clearButton.addEventListener('click',function(event){
           playerTwoGuess.value = "";
           playerOneName.value = ""; 
           playerTwoName.value = ""; 
-  
+          disableButton[1].disabled = true;
+          disableButton[2].disabled = true;
 }); 
 
 
-function appendCard(e){
-  e.preventDefault();
+
+
+
+function appendCard(){
   var card = `<article class="card result-card">
               <div class="player-names"><h1>${playerOneName.value}<span>vs</span> ${playerTwoName.value}</h1></div>
               <hr>
               <div class="winner-name">
                   <div>
-                    <h1 class="winner-player-name"></h1>
+                    <h1 class="winner-player-name"> </h1>
                     <p class="winner">Winner</p>
                 </div>
               </div>
@@ -104,10 +136,35 @@ function deleteCard() {
 function getWinner(){
   if(playerOneGuess.value == randomNumber){
     document.querySelector('.winner-player-name').innerHTML = playerOneName.value;
-  } else if(playerTwoGuess.value == randomNumber){
+  } else if (playerTwoGuess.value == randomNumber){
     document.querySelector('.winner-player-name').innerHTML = playerTwoName.value;
   }
 }
+
+
+function randomNumberFunction() {
+  var randomNumberGenerator = Math.floor(Math.random() * (parseInt(maxRange.value) - parseInt(minRange.value) + 1)) + parseInt(minRange.value); 
+  return randomNumberGenerator;
+};
+
+function errorDisplay(){
+  if(playerOneGuess.value === "" || playerTwoGuess.value === ""){
+            var element = document.querySelectorAll('.error-display');
+            for(var i=0; i<element.length;i++){
+            element[i].classList.remove('error-display');}
+            for (var i=0; i<disableButton.length;i++){
+            disableButton[1].disabled = true;
+            disableButton[2].disabled = true;
+            }
+
+          }
+};
+
+
+
+
+
+
 
 
 
